51e87cea0cd6992d9af8639dd8902904afa1a597
stormytuna <stormytuna@outlook.com>
2023-02-18 01:35:08 +0000

    Some API setup stuff

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
+from flask import Flask
+
+load_dotenv()
+
+app = Flask(__name__)
+url = os.getenv("DATABASE_URL")
+connection = psycopg2.connect(url)
+
+@app.get("/")
+def home():
+    return { "data": "Hello world!"}
